a59b505c8c1dc26bd38d1514c8cad97e49112145
Duy Nguyen <nkduy2010@gmail.com>
2025-03-10 16:59:04 -0400

    init commit

---
+++ data-access/src/functions/api.ts
@@ -0,0 +1,35 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function apiHandler(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  const url = new URL(request.url);
+  const path = url.pathname.split("/").pop().toLowerCase();
+
+  if (path === "nocachehint") {
+    return { body: new Date().toISOString() };
+  } else if (path === "cachecontrolwithmaxage") {
+    return {
+      body: new Date().toISOString(),
+      headers: {
+        "Cache-Control": "max-age=20",
+      },
+    };
+  } else if (path === "cachecontrolprivatenostore") {
+    return {
+      body: new Date().toISOString(),
+      headers: {
+        "Cache-Control": "no-store, private",
+      },
+    };
+  } else {
+    return { status: 404, body: "Not Found" };
+  }
+}
+
+app.http("cacheControl", {
+  methods: ["POST"],
+  route: "cacheControl/{*segments}",
+  authLevel: "anonymous",
+  handler: apiHandler,
+});
