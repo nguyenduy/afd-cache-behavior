3d7dbac7962f414ccead00f581fcda4943d2f824
Duy Nguyen <nkduy2010@gmail.com>
2025-04-01 14:13:40 -0400

    add buttons to call Microsoft endpoints

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -7,33 +7,48 @@ function App() {
   const aFDCacheEnabledEndpoint = import.meta.env.VITE_AFD_ENDPOINT_CACHE_ENABLED as string;
   const aFDCacheDisabledEndpoint = import.meta.env.VITE_AFD_ENDPOINT_CACHE_DISABLED as string;
 
+  const microsoftEndpointThroughFD = import.meta.env.VITE_MICROSOFT_FD_ENDPOINT as string;
+  const microsoftEndpointDirectlyToFunctionApp = import.meta.env.VITE_MICROSOFT_FUNCTION_ENDPOINT;
+  
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <Row>
         <Row style={{ minHeight: "20vh", width: "100vw" }}>
-          <Col span={6}>
+          <Col span={4}>
             <div style={{ textAlign: "center", height: "50px" }}>
               <h4>Directly to Function App</h4>
             </div>
             <ApiResult uri={functionAppEndpoint} />
           </Col>
-          <Col span={6}>
+          <Col span={4}>
             <div style={{ textAlign: "center", height: "50px" }}>
               <h4>Through AFD with Cache enabled</h4>
             </div>
             <ApiResult uri={aFDCacheEnabledEndpoint} />
           </Col>
-          <Col span={6}>
+          <Col span={4}>
             <div style={{ textAlign: "center", height: "50px" }}>
               <h4>Through AFD with Cache disabled by a rule set</h4>
             </div>
             <ApiResult uri={aFDCacheEnabledEndpoint} cacheDisabledThroughRuleSet={true} />
           </Col>
-          <Col span={6}>
+          <Col span={4}>
             <div style={{ textAlign: "center", height: "50px" }}>
               <h4>Through AFD with Cache Disabled</h4>
             </div>
             <ApiResult uri={aFDCacheDisabledEndpoint} />
+          </Col>
+          <Col span={4}>
+            <div style={{ textAlign: "center", height: "50px" }}>
+              <h4>New button from Microsoft-Through front door</h4>
+            </div>
+            <ApiResult uri={microsoftEndpointThroughFD} />
+          </Col>
+          <Col span={4}>
+            <div style={{ textAlign: "center", height: "50px" }}>
+              <h4>New button from Microsoft-directly to function app</h4>
+            </div>
+            <ApiResult uri={microsoftEndpointDirectlyToFunctionApp} />
           </Col>
         </Row>
       </Row>
